6145617b65ece150179619d885bd7655fa09e15a
dammy <hemmanueldami@gmail.com>
2024-04-15 13:59:30 +0100

    Footer Added

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,13 @@
+<footer>
+    <div class="footer_links">
+        
+    </div>
+    <br>
+        <hr>
+            © 2024 · FIBE Business Agency
+        <hr>
+    <br>
+</footer>
 
 <script src="assets/js/main.js"></script>
 </body>
