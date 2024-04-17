c37fc11db631b60c3dacf1e22a78d4fc0217b2e5
dammy <hemmanueldami@gmail.com>
2024-04-17 18:31:16 +0100

    Views Counter Fixed and Like Button

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta name="google-site-verification" content="hXYajpy04IHMgYZI5QqAgW9jkjPap9lFdjI6dTxL870" />
+
     <meta http-equiv="Cache-control" content="public">
     <meta http-equiv="Cache-control" content="private">
     <meta http-equiv="Cache-control" content="no-cache">
