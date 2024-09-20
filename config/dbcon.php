d7b620394fabf58d81ae674fe9c089fca9fef3f8
Damilare <sdesparkflux@gmail.com>
2024-09-20 14:00:49 +0100

    Fresh Start

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,14 +1,14 @@
 <?php
 
-    // $host = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $database = "fibe";
-
     $host = "localhost";
-    $username = "alphonzt_fibe";
-    $password = "Trodpen2022*";
-    $database = "alphonzt_fibe";
+    $username = "root";
+    $password = "";
+    $database = "fibe";
+
+    // $host = "localhost";
+    // $username = "alphonzt_fibe";
+    // $password = "Trodpen2022*";
+    // $database = "alphonzt_fibe";
 
     $con = mysqli_connect($host, $username, $password, $database);
 
